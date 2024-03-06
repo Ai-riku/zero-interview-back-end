e4492a19b0910d6429b166790171cb90f7e7dee2
Ai-riku <116927374+Ai-riku@users.noreply.github.com>
2024-03-05 21:37:17 -0800

    Added audio recording functionality

--- app.py
+++ app.py
@@ -1,34 +1,7 @@
 from flask import Flask, render_template, Response
-import cv2
-import time
+from av_util import video_capture
 
 app=Flask(__name__)
-
-def video_capture():
-    camera=cv2.VideoCapture(0)
-    frame_width = int(camera.get(3)) 
-    frame_height = int(camera.get(4)) 
-    size = (frame_width, frame_height)
-    fps = camera.get(cv2.CAP_PROP_FPS) 
-    out = cv2.VideoWriter('output.avi',  
-                         cv2.VideoWriter_fourcc(*'MJPG'), 
-                         fps, size) 
-    start = time.time()
-    end = start
-    while end - start < 5:
-        ## read the camera frame
-        success, frame = camera.read()
-        if not success:
-            break
-        out.write(frame)
-        ret, buffer = cv2.imencode('.jpg',frame)
-        frame = buffer.tobytes()
-        yield(b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-        end = time.time()
-    camera.release()
-    out.release()
-    print("Video saved!")
 
 @app.route('/')
 def index():
---
+++ av_util.py
@@ -0,0 +1,151 @@
+from __future__ import print_function, division
+from moviepy.editor import VideoFileClip, AudioFileClip
+import cv2
+import pyaudio
+import wave
+import threading
+import time
+import os
+
+class VideoRecorder():  
+    def __init__(self, name="temp_video.avi", camindex=0, fourcc="MJPG"):
+        self.open = True
+        self.device_index = camindex
+        self.video_filename = name
+        self.video_cap = cv2.VideoCapture(self.device_index)
+        self.frame_width = int(self.video_cap.get(3))
+        self.frame_height = int(self.video_cap.get(4)) 
+        self.frameSize = (self.frame_width, self.frame_height) 
+        self.current_frame = None
+        self.fps = self.video_cap.get(cv2.CAP_PROP_FPS)
+        self.video_writer = cv2.VideoWriter_fourcc(*fourcc)
+        self.video_out = cv2.VideoWriter(self.video_filename, self.video_writer, self.fps, self.frameSize)
+        self.frame_counts = 1
+        self.start_time = time.time()
+
+    def record(self):
+        while self.open:
+            ret, video_frame = self.video_cap.read()
+            if not ret: continue
+            self.video_out.write(video_frame)
+            self.current_frame = video_frame
+            self.frame_counts += 1
+                
+    def stop(self):
+        if self.open:
+            self.open=False
+            self.video_out.release()
+            self.video_cap.release()
+            cv2.destroyAllWindows()
+
+    def start(self):
+        video_thread = threading.Thread(target=self.record)
+        video_thread.start()
+
+class AudioRecorder():
+    def __init__(self, filename="audio_output.wav", rate=44100, fpb=1024, channels=2):
+        self.open = True
+        self.rate = rate
+        self.frames_per_buffer = fpb
+        self.channels = channels
+        self.format = pyaudio.paInt16
+        self.audio_filename = filename
+        self.audio = pyaudio.PyAudio()
+        self.stream = self.audio.open(format=self.format,
+                                      channels=self.channels,
+                                      rate=self.rate,
+                                      input=True,
+                                      frames_per_buffer = self.frames_per_buffer)
+        self.audio_frames = []
+
+    def record(self):
+        self.stream.start_stream()
+        while self.open:
+            data = self.stream.read(self.frames_per_buffer) 
+            self.audio_frames.append(data)
+            if not self.open:
+                break
+
+    def stop(self):
+        if self.open:
+            self.open = False
+            self.stream.stop_stream()
+            self.stream.close()
+            self.audio.terminate()
+            waveFile = wave.open(self.audio_filename, 'wb')
+            waveFile.setnchannels(self.channels)
+            waveFile.setsampwidth(self.audio.get_sample_size(self.format))
+            waveFile.setframerate(self.rate)
+            waveFile.writeframes(b''.join(self.audio_frames))
+            waveFile.close()
+
+    def start(self):
+        audio_thread = threading.Thread(target=self.record)
+        audio_thread.start()
+
+def start_AVrecording():
+    global video_thread
+    global audio_thread
+    video_thread = VideoRecorder()
+    audio_thread = AudioRecorder()
+    audio_thread.start()
+    video_thread.start()
+    return video_thread
+
+def start_video_recording(filename="test"):
+    global video_thread
+    video_thread = VideoRecorder()
+    video_thread.start()
+    return filename
+
+def start_audio_recording(filename="test"):
+    global audio_thread
+    audio_thread = AudioRecorder()
+    audio_thread.start()
+    return filename
+
+def stop_AVrecording(filename="test"):
+    audio_thread.stop() 
+    frame_counts = video_thread.frame_counts
+    elapsed_time = time.time() - video_thread.start_time
+    recorded_fps = frame_counts / elapsed_time
+    print("total frames " + str(frame_counts))
+    print("elapsed time " + str(elapsed_time))
+    print("recorded fps " + str(recorded_fps))
+    video_thread.stop() 
+
+    # Makes sure the threads have finished
+    while threading.active_count() > 3:
+        time.sleep(1)
+    
+    # Merging audio and video signal    
+    video_file_path = 'temp_video.avi'
+    audio_file_path = 'audio_output.wav'
+    video_clip = VideoFileClip(video_file_path).set_fps(recorded_fps)
+    audio_clip = AudioFileClip(audio_file_path)
+    synchronized_clip = video_clip.set_audio(audio_clip)
+    synchronized_clip.write_videofile(filename, codec="libx264", audio_codec="aac")
+
+def file_manager(filename="test"):
+    "Required and wanted processing of final files"
+    local_path = os.getcwd()
+    if os.path.exists(str(local_path) + "/temp_video.avi"):
+        os.remove(str(local_path) + "/temp_video.avi")
+    if os.path.exists(str(local_path) + "/" + filename + ".avi"):
+        os.remove(str(local_path) + "/" + filename + ".avi")
+
+def video_capture():
+    record_duration = 5.0
+    start_AVrecording()
+    start_time = time.time()
+    while (time.time() - start_time) < record_duration:
+        frame = video_thread.current_frame
+        if frame is not None:
+            ret, buffer = cv2.imencode('.jpg',frame)
+            frame = buffer.tobytes()
+            yield(b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+    stop_AVrecording("av_output.avi")
+    file_manager()
+
+
